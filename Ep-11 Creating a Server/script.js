f63be8ebd7c91ddb11fc269213f4e46cf842c165
Roshan Roy <roshanroy1121@gmail.com>
2025-07-01 17:04:24 +0530

    added more info

--- Ep-11 Creating a Server/script.js
+++ Ep-11 Creating a Server/script.js
@@ -10,3 +10,16 @@ const server = http.createServer(function (req, res) {
 });
 
 server.listen(3001);
+
+/*
+THINGS I LEARN IN THIS TUTORIAL
+* Hardware
+* Software
+* Protocols: TCP/IP, HTTP, FTP, SMTP
+* What is a protocol?
+* What happens behind the scenes when a user makes a request? => (hints:- dns, socket connection, small packets)
+* How data is send back to client from server?
+* PORT
+* Socket vs Websocket
+* Creating server in NodeJS.
+*/
