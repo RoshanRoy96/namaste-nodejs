48b2754400408df9ed6b4d05e8220494c645d150
Roshan Roy <roshanroy1121@gmail.com>
2025-07-02 16:56:30 +0530

    added new folder

---
+++ Ep-12 Databases-SQL & NoSQL/script.js
@@ -0,0 +1,19 @@
+/*
+1) What is database?
+Database is an organized collection of data.
+
+2) What is DBMS?
+DBMS stands for Data Base Management System.
+It is a way to interact with data base.
+eg:- adding data to database, fetching data from database.
+
+3) Types of Data Base
+Relational Data Base- MySQL, PostgreSQL
+NoSQL Data Base- MongoDB
+In memory DB- Redis
+
+Why MongoDB is popular?
+Flexible, compatable with JS stack, datas are stored as documents, increases developer productivity.
+
+
+*/ 
