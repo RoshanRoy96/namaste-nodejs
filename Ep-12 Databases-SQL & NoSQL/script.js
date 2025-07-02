0cf1de4d411aa4d44f3349fd2d03554dc1b26d38
Roshan Roy <roshanroy1121@gmail.com>
2025-07-02 17:03:01 +0530

    added new folder

--- Ep-12 Databases-SQL & NoSQL/script.js
+++ Ep-12 Databases-SQL & NoSQL/script.js
@@ -15,5 +15,12 @@ In memory DB- Redis
 Why MongoDB is popular?
 Flexible, compatable with JS stack, datas are stored as documents, increases developer productivity.
 
+What is schema?
+Schema means what are the details inside RDBMS or NoSQL.
 
+Comparison between RDBMS and NoSQL?
+In RDBMS datas are stored inside table while in NoSQL datas are stored as documents.
+In RDBMS datas are structured while in NoSQL datas are not structured.
+In RDBMS there is fixed schema while in NoSQL flexible schema.
+In RDBMS horizontal scaling is tough while in NoSQL easy to scale horizontally and vertically.  
 */ 
