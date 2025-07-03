c62a6cb173e7935bae241d3aa69996ef016d3482
Roshan Roy <roshanroy1121@gmail.com>
2025-07-03 22:50:29 +0530

    added new folder

---
+++ Ep-13 Creating a database & mongodb/database.js
@@ -0,0 +1,12 @@
+/*
+NOTES
+* Go to mongodb website
+* Create a free M0 cluster
+* Create a user (user: roshanroy1121, password: 78ej92VZmo5ObNsR)
+* Get the connection string like below.
+* Install mongodb compass
+*/ 
+
+require("mongodb");
+
+const URI = "mongodb+srv://roshanroy1121:78ej92VZmo5ObNsR@mern-vercel.r56h0.mongodb.net/";
