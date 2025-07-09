74941ab8ebf2c111627c9f113f79735502baf439
Roshan Roy <roshanroy1121@gmail.com>
2025-07-09 07:50:19 +0530

    added new folder

---
+++ Season-2/Ep-02 Features, HLD, LLD & Planning/script.js
@@ -0,0 +1,11 @@
+/*
+FEATURES OF OUR APP
+* Create an account(Signup)
+* Login
+* Create profile/Update profile
+* Feed page => explore other users 
+* send connection request
+* see our matches
+* see request we have sent/received
+* update your profile
+*/
