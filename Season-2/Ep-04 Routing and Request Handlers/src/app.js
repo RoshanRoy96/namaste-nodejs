12c71e60e8f0ee229a59c4124d652b62e526953d
Roshan Roy <roshanroy1121@gmail.com>
2025-07-11 08:21:59 +0530

    added some code

--- Season-2/Ep-04 Routing and Request Handlers/src/app.js
+++ Season-2/Ep-04 Routing and Request Handlers/src/app.js
@@ -2,12 +2,41 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  console.log("Hello, hello, hello");
-}); 
+// app.use("/", (req, res) => {
+//   res.send("Namaste");
+// });
+
+// app.use("/hello/2", (req, res) => {
+//   res.send("Hello-2");
+// });
+
+// app.use("/hello", (req, res) => {
+//   res.send("Hello, hello, hello");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send("Namaste");
+// });
+
+app.use("/user", (req, res) => {
+  res.send("order matters");
+});
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Roshan", lastName: "Roy" });
+});
 
 app.use("/test", (req, res) => {
-  res.send("Hello from server");
+  res.send("Hello from the server");
+});
+
+app.post("/user", (req, res) => {
+  console.log("Save data to database");
+  res.send("Data seccessfully saved to database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
 });
 
 app.listen(3000, () => {
