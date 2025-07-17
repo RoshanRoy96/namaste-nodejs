157c96e35ef87efba16153ac3f4fb4a68be94bda
Roshan Roy <roshanroy1121@gmail.com>
2025-07-17 13:00:26 +0530

    database basics

--- Season-2/Ep-06 Database Schema and Models-Mongoose/src/app.js
+++ Season-2/Ep-06 Database Schema and Models-Mongoose/src/app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
+const { User } = require("./models/user");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "Roshan",
+    lastName: "Roy",
+    emailId: "roshanroy1121@gmail.com",
+    password: "roshan@1234",
+  };
+
+  // creating a new instance of User model
+  const user = new User(userObj);
+
+  await user.save(); // saving data to our database....this function will returns a promise...so we have to use async-await here
+  res.send("User added successfully");
+});
 
 connectDB()
   .then(() => {
--- Season-2/Ep-06 Database Schema and Models-Mongoose/topics.md
+++ Season-2/Ep-06 Database Schema and Models-Mongoose/topics.md
@@ -3,3 +3,6 @@
 - connect application to the database "connection-url"/devTinder
 - call connect DB function and connect to application before listening to port 7777
 - create a userSchema and user Model
+- create POST "/signup" api to add data to database
+- push some documents using api calls from postman
+- error handling using try-catch
