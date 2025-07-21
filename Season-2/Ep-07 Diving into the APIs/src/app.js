acd0600474c389aa9cd4c1e11151622c14c3c769
Roshan Roy <roshanroy1121@gmail.com>
2025-07-21 13:01:34 +0530

    added API using GET method

--- Season-2/Ep-07 Diving into the APIs/src/app.js
+++ Season-2/Ep-07 Diving into the APIs/src/app.js
@@ -16,6 +16,44 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// app.get("/user", async (req, res) => {
+//   const userEmail = req.body.emailId;
+
+//   try {
+//     const users = await User.find({ emailId: userEmail });
+//     if (users.length === 0) {
+//       res.status(404).send("User not found");
+//     } else {
+//       res.send(users);
+//     }
+//   } catch (err) {
+//     res.status(400).send("Something went wrong");
+//   }
+// });
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if(!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
