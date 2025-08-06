50f503daed550efcde59ec5cea413314c096b647
Roshan Roy <roshanroy1121@gmail.com>
2025-08-06 10:38:27 +0530

    added new folder

---
+++ Season-2/Ep-11 Diving into the API's and Express Router/src/app.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const { connectDB } = require("./config/database");
+const { User } = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const { userAuth } = require("./middlewares/auth");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser()); // for reading cookies
+
+app.post("/signup", async (req, res) => {
+  try {
+    // validation of data
+    validateSignUpData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+
+    // encrypting password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // creating new instance of user
+    const userObj = { firstName, lastName, emailId, password: passwordHash };
+    const user = new User(userObj);
+
+    await user.save();
+    res.send("User added successfully");
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      res.send("Invalid credentials");
+    }
+    const isPasswordValid = await user.validatePassword(password);
+    if (isPasswordValid) {
+      // creating JWT token
+      const token = await user.getJWT();
+
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 24 * 60 * 60 * 1000),
+      }); // cookie will expire in 8 days
+      res.send("Login Successful!");
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+app.get("/profile", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+  const user = req.user;
+  console.log("Sending connection request");
+  res.send(user.firstName + " " + "sent connection request");
+});
+
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+    app.listen(7777, () => {
+      console.log("Server listening on port 7777");
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed");
+  });
---
+++ Season-2/Ep-11 Diving into the API's and Express Router/src/models/user.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      lowercase: true, // if a user adds emailId in capital letters, this will converts that to small letters(eg: ROshan@gmail.com)
+      required: true,
+      unique: true,
+      trim: true, // if a user adds emailId by putting some space before and after, this will trim of that(eg: "emailId": "   roshan@gmail.com   ");
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address:" + value);
+        }
+      },
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.pnrao.com/wp-content/uploads/2023/06/dummy-user-male.jpg",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid image URL: " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.getJWT = async function () {
+  const user = this;
+
+  const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", {
+    expiresIn: "7d",
+  });
+  return token;
+};
+
+userSchema.methods.validatePassword = async function(passwordInputByUser) {
+  const user = this;
+  const passwordHash = user.password;
+
+  const isPasswordValid = await bcrypt.compare(passwordInputByUser, passwordHash);
+
+  return isPasswordValid;
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
