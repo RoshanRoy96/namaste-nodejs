6c5e1927ae3868093ae81b9c43247eb2a2277261
Roshan Roy <roshanroy1121@gmail.com>
2025-09-01 11:05:56 +0530

    added sendConnectionRequest API

--- Season-2/Ep-12 Logical DB Query and Compound Indexes/src/routes/requestRouter.js
+++ Season-2/Ep-12 Logical DB Query and Compound Indexes/src/routes/requestRouter.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const { ConnectionRequestModel } = require("../models/connectionRequest");
 
 const requestRouter = express();
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  console.log("Sending connection request");
-  res.send(user.firstName + " " + "sent connection request");
-});
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params?.toUserId;
+      const status = req.params?.status;
+
+      const connectionRequest = new ConnectionRequestModel({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: `Connection request sent successfully`,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("Error: " + err.message);
+    }
+  }
+);
 
 module.exports = { requestRouter };
